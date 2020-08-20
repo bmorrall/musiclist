cec116cfeeb262266a63dd3b8faa579ad1c40098
Ben Morrall <bemo56@hotmail.com>
2020-08-20 16:00:14 +1000

    Added navigation by year

--- app/controllers/albums/years_controller.rb
+++ app/controllers/albums/years_controller.rb
@@ -15,8 +15,16 @@ module Albums
     def show
       authorize Album
       @year = params[:id]
+      @previous_year = all_years.first != @year && all_years[all_years.index(@year) - 1]
+      @next_year = all_years.last != @year && all_years[all_years.index(@year) + 1]
       @albums = policy_scope(Album).where(year: @year).order(:year, :title)
       @albums = present(@albums, AlbumDecorator)
+    end
+
+    private
+
+    def all_years
+      @all_years ||= policy_scope(Album).where.not(year: [nil, ""]).pluck(:year).sort
     end
   end
 end
